ad7b3bb0e4fe43484ffa0509664dc72a80a5c9d3
Polina Katunina <p.katunina@macbook-jjh19jjwv1.int.bksp.network>
2024-09-28 22:41:29 +0300

    add zellij

--- fish.nix
+++ fish.nix
@@ -5,26 +5,27 @@
     fish
     grc
     fishPlugins.grc
-    fishPlugins.z
+    zoxide
   ];
 
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+      zoxide init fish | source
+      alias cd z
     '';
-  plugins = [
-    {name = "grc"; src = pkgs.fishPlugins.grc;}
-    {name = "z"; src = pkgs.fishPlugins.z;}
-    {
-      name = "tide";
-      src = pkgs.fetchFromGitHub {
-        owner = "IlanCosman";
-        repo = "tide";
-        rev = "a34b0c2809f665e854d6813dd4b052c1b32a32b4";
-        sha256 = "sha256-ZyEk/WoxdX5Fr2kXRERQS1U1QHH3oVSyBQvlwYnEYyc=";
+    plugins = [
+      {name = "grc"; src = pkgs.fishPlugins.grc;}
+      {
+        name = "tide";
+        src = pkgs.fetchFromGitHub {
+          owner = "IlanCosman";
+          repo = "tide";
+          rev = "a34b0c2809f665e854d6813dd4b052c1b32a32b4";
+          sha256 = "sha256-ZyEk/WoxdX5Fr2kXRERQS1U1QHH3oVSyBQvlwYnEYyc=";
         };
       }
-    ];
+    ]; 
   };
 }
