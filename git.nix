daace1305f86569f9c95351866ea78dabbbff2bd
Polina Katunina <katunina0polina@gmail.com>
2024-09-30 23:20:53 +0300

    improve git config

--- git.nix
+++ git.nix
@@ -2,12 +2,18 @@
 {
     programs.git = {
         enable = true;
+        userName = "Polina Katunina";
         includes = [
             {
                 condition = "gitdir:~/.config/";
                 contents.user = {
                     email = "katunina0polina@gmail.com";
-                    name = "Polina Katunina";
+                };
+            }
+            {
+                condition = "gitdir:~/work/";
+                contents.user = {
+                    email = "p.katunina@tbank.ru";
                 };
             }
         ];
